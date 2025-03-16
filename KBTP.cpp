df9cd727dac63f102e7ddcb09b442605536a214f
kbtelles <kbtelles@scl.edu.gt>
2025-03-16 11:54:07 -0600

    agregar clase KBTP

---
+++ KBTP.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+class KBTP{
+	private :
+	            string cui, nombres, apellidos;
+	            
+	            
+	    void crear();
+	    void leer();
+	    void actualizar();
+	    void borrar();
+
+}; 
