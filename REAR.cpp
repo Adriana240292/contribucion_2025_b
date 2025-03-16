095093b07082e98d8f28241383cec26c68ab0c10
unknown <ricardoarrecisrivera@gmail.com>
2025-03-16 11:54:13 -0600

    Se sube clase REAR

---
+++ REAR.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+class REAR{
+	private :
+		string cui,nombres,apellidos;
+		
+		void crear();
+		void leer();
+		void eliminar();
+		void actualizar();
+};
