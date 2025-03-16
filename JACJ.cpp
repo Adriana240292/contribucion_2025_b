0fc14b16587ffcbaeb56bcdc4afea6bbd7ee89bf
Adriana240292 <jcatalan@miumg.edu.gt>
2025-03-16 11:54:07 -0600

    agregar clase JACJ

---
+++ JACJ.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+class JACJ{
+	private :
+		string cui,nombres,apellidos;
+		
+		void crear();
+		void leer();
+		void actualizar();
+		void borrar();
+		
+	
+};
