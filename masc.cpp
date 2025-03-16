2817f2d57452f3d47d3ffafc57dae4f2b726e4c5
M@rc0_Sama <130240200+Mrc0sc11@users.noreply.github.com>
2025-03-16 11:56:09 -0600

    add class MASC

---
+++ masc.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class MASC{
+    private :
+    string cui, nombres, apellidos;
+
+    void crear();
+    void actualizar();
+    void leer();
+    void borrar();
+
+};
